949c6ddd8c691f2068f101b581d681e6ca54eb7b
ElandaDRM <89924166+ElandaDRM@users.noreply.github.com>
2026-02-24 22:38:43 +0800

    fix:修复模块引用

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,17 @@
+mod auth;
+mod handler;
+use auth::official::add_new_account;
+use auth::official::check_account_time;
+use auth::official::download_player_skin;
+use auth::littleskin::useMethod;
+use auth::yggdrasil::thirdPartyLogin;
+use handler::authSearcher::get_all_accounts;
+use handler::authSearcher::get_all_littleskin_users;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![add_new_account,check_account_time,download_player_skin,useMethod,thirdPartyLogin,get_all_accounts,get_all_littleskin_users])
     .setup(|app| {
       if cfg!(debug_assertions) {
         app.handle().plugin(
